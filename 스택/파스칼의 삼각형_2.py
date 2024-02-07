de6295e78da961591cbf88db8e9f10b8a25466ee
growbori <grow.boribori@gmail.com>
2024-02-07 21:16:28 +0900

    0207

--- 스택/파스칼의 삼각형_2.py
+++ 스택/파스칼의 삼각형_2.py
@@ -1,15 +1,14 @@
-arr = [[0]*(i+1) for i in range(4)]
-
 T = int(input())
 
 for tc in range(T):
     n = int(input())
-    for i in range(0, n+1):
+    arr = [[1] * (i + 1) for i in range(n)]
+    for i in range(0, n):
         for j in range(0, i):
-            if j == 0 or i == j:
-                arr[i][j] = 1
 
-            else:
-                arr[i][j] = arr[i-1][j-1]+ arr[i-1, j]
+            if j != 0 and i != j:
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
 
-print(arr)
+print(f'#{tc+1}')
+for i in range(1, n):
+    print(*arr[i])
